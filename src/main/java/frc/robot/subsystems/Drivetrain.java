1b353ef8009bc911f128e117cbb4c3dfd7a97bcd
FinkleWrinkle <supergamerg99@gmail.com>
2023-01-14 15:55:29 -0500

    unfished odometry and kinematics

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -9,8 +9,13 @@ import com.kauailabs.navx.frc.AHRS;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.SPI.Port;
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
+import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
+import edu.wpi.first.math.kinematics.MecanumDriveKinematics;
+import edu.wpi.first.math.kinematics.MecanumDriveOdometry;
 
 public class Drivetrain extends SubsystemBase {
   /** Creates a new Drivetrain. */
@@ -18,10 +23,29 @@ public class Drivetrain extends SubsystemBase {
   private final WPI_TalonFX rightRear = new WPI_TalonFX(2);
   private final WPI_TalonFX leftRear = new WPI_TalonFX(5);
   private final WPI_TalonFX rightFront = new WPI_TalonFX(4);
+  
+  Encoder encoder = new Encoder(0,1);
+
+  Translation2d m_frontLeftLocation = new Translation2d(0.305, 0.257175);
+  Translation2d m_frontRightLocation = new Translation2d(-0.305, 0.257175);
+  Translation2d m_backLeftLocation = new Translation2d(0.305, -0.257175);
+  Translation2d m_backRightLocation = new Translation2d(-0.305, -0.257175);  
+
+  public static final double kGearRatio = 10.71;
+  public static final double kWheelRadius = 0.076;
+  public static final double kEncoderResolution = 2048;
+  public static final double kDistancePerTick = (2 * Math.PI * kWheelRadius / kEncoderResolution) / kGearRatio;
+
+
+
+  private final MecanumDriveKinematics kinematics = new MecanumDriveKinematics(m_backLeftLocation, m_backRightLocation, m_frontLeftLocation, m_frontRightLocation);
 
   private final MecanumDrive drivetrain = new MecanumDrive(leftFront, leftRear, rightFront, rightRear);
   private  AHRS navx;
   private Rotation2d initAngle;
+
+  //private final MecanumDriveOdometry odometry = new MecanumDriveOdometry(kinematics, navx.getRotation2d(), new Mechanism2d(rightFront.getse)
+
 
 
 public Drivetrain(){
@@ -41,6 +65,7 @@ public Drivetrain(){
   rightRear.configNominalOutputReverse(0);
   leftFront.configNominalOutputReverse(0);
   leftRear.configNominalOutputReverse(0);
+  // odometry = new MechanumDriveOdomotry
 }
   
   public void driveMecanum(double x, double y, double rotation) {
@@ -50,6 +75,16 @@ public Drivetrain(){
 
   public void zero() {
     initAngle = navx.getRotation2d();
+  }
+
+  public double getDistance() {
+    double leftMotor = leftFront.getSelectedSensorPosition() * kDistancePerTick;
+    double rightMotor = -rightFront.getSelectedSensorPosition() * kDistancePerTick;
+
+    double averageMove = (leftMotor + rightMotor) / 2;
+
+    return -averageMove;
+
   }
 
   // public void drive(double x, double y, double z) {
